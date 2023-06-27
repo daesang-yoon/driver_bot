0af3a80085408ca3c391380cfc301c49b2ad4084
Dae Sang Yoon <ryanyoon3@gmail.com>
2023-06-27 00:17:24 -0700

    started on google sheets api

---
+++ assign.py
@@ -0,0 +1,20 @@
+import os
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+SCOPES = ["https://googleapis.com/auth/spreadsheets"]
+SPREADSHEET_ID = "18MYt45bq_UjoAKP-cQjg5nVeyAXaC7lpC_CsUIKXJns"
+
+
+def assign_rides_back():
+    
+    credentials = None
+    
+
+
+    return ''
